a08216ce0b450944cde91ea3d501f6a66656b355
Igor <91732297+FaFoLu@users.noreply.github.com>
2022-09-20 23:32:25 +0300

    Update bot.js
    test-update

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const { Bot, Keyboard } = require ('grammy');
-const text = require('./text')
+const text = require('./text');
 require('dotenv').config();
 
 const bot = new Bot(process.env.BOT_TOKEN);
@@ -7,7 +7,7 @@ const bot = new Bot(process.env.BOT_TOKEN);
 bot.catch((error) => {
     const ctx = error.ctx;
     console.log(`Error while handling update: ${ctx.message.text}`)
-})
+});
 
 const keyboard = new Keyboard()
   .text("🔓Створіть простий пароль").row()
@@ -17,21 +17,53 @@ const keyboard = new Keyboard()
 
 bot.command('start', async (ctx) => {
     await ctx.reply(text.textWelcome, {
+        reply_to_message_id: ctx.msg.message_id,
         reply_markup: keyboard,
         disable_web_page_preview: true,
         parse_mode: 'HTML',
     })
 });
 
+
+const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 bot.on('message:text', async (ctx) => {
+
     if (ctx.message.text == '🔓Створіть простий пароль') {
-        await ctx.reply('Твій пароль:')
+
+        var passwordLength = 5;
+        var password = "";
+        
+        for (var i = 0; i <= passwordLength; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            password += chars.substring(randomNumber, randomNumber +1);
+        }
+
+        await ctx.reply('Твій пароль: ' + password)
 
     } else if (ctx.message.text == '🔒Створіть середній пароль') {
-        await ctx.reply('Твій пароль:')
+
+        var passwordLength = 8;
+        var password = "";
+        
+        for (var i = 0; i <= passwordLength; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            password += chars.substring(randomNumber, randomNumber +1);
+        }
+
+        await ctx.reply('Твій пароль: ' + password)
         
     } else if (ctx.message.text == '🔐Створіть надійний пароль') {
-        await ctx.reply('Твій пароль:')
+
+        var passwordLength = 12;
+        var password = "";
+        
+        for (var i = 0; i <= passwordLength; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            password += chars.substring(randomNumber, randomNumber +1);
+        }
+
+        await ctx.reply('Твій пароль: ' + password)
     } else {
         console.log('Else')
     }    
