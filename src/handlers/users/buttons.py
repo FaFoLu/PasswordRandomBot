64c316c396e3181d273a5d9e6fc3ca9e22d8dada
Igor <91732297+FaFoLu@users.noreply.github.com>
2022-11-15 11:58:08 +0300

    1.0.1
    Update src/handlers/users/buttons.py

--- src/handlers/users/buttons.py
+++ src/handlers/users/buttons.py
@@ -1,14 +1,21 @@
 from aiogram import types
 from loader import dp
 
+from random import sample
+import string
+
+randomPassword_1 = ''.join(sample(string.ascii_uppercase + string.ascii_lowercase, k=5))
+randomPassword_2 = ''.join(sample(string.ascii_uppercase + string.ascii_lowercase, k=8))
+randomPassword_3 = ''.join(sample(string.ascii_uppercase + string.ascii_lowercase, k=11))
+
 @dp.message_handler(text='🔓Створіть простий пароль')
 async def buttons(message: types.Message):
-    await message.answer('Твій пароль:')
+    await message.answer(f"Твій пароль: <code>{randomPassword_1}</code>")
 
 @dp.message_handler(text='🔒Створіть середній пароль')
 async def buttons(message: types.Message):
-    await message.answer('Твій пароль:')
+    await message.answer(f"Твій пароль: <code>{randomPassword_2}</code>")
 
 @dp.message_handler(text='🔐Створіть надійний пароль')
 async def buttons(message: types.Message):
-    await message.answer('Твій пароль:')
+    await message.answer(f"Твій пароль: <code>{randomPassword_3}</code>")
